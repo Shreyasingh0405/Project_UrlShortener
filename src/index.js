229312717751d8789529d763d5149abf209ead6b
shreya <shreyasingh0405.52@gmail.com>
2022-05-17 16:34:05 +0530

    phase 1

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://Project2_GroupNo_103:Project2_GroupNo_103@cluster0.dwdte.mongodb.net/groupXdatabase", {
+mongoose.connect("mongodb+srv://lddu818:27o3D6VwW2z1zHMj@cluster0.6gomf.mongodb.net/group11Database", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,12 @@
 const express = require('express');
 
 const router = express.Router();
+const urlController = require("../controller/urlController")
 
+
+
+router.post('/url/shorten', urlController.shortUrl);
+
+router.get('/:urlCode', urlController.getShortUrl);
 
 module.exports = router;
